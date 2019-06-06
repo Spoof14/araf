a8a58bb419a681439cccf5f9cdf2a1a727ec8204
Lars <lars.manden@gmail.com>
2019-06-06 19:59:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import champions from './champion.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			roles: ["Top", "Jungle", "Mid", "Bottom", "Support"]
+		}
+	}
+
+	render() {
+		let divs = [];
+		this.rollChampions(5).forEach(champion => {
+			divs.push(
+				<div key={champion.element.name} style={{display:'flex', flexDirection:'column', margin:'1rem'}}>
+				
+					<span>{champion.element.name}</span>
+					<img src={`${process.env.PUBLIC_URL}/champion/${champion.element.image.full}`} height='100px' width='100px' alt="champion"></img>
+					<span>{champion.role}</span>
+				</div>
+			)
+		})
+		return (
+			<div className="App" style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
+				{divs}
+			</div>
+		);
+	}
+
+
+	rollChampions(numberOfChampions) {
+		let champs = new Map();
+		while(champs.size < numberOfChampions){
+			var random = Math.floor(Math.random() * Object.keys(champions.data).length + 1);
+			var key = Object.keys(champions.data)[random];
+			var element = champions.data[key];
+			champs.set(element.name, {element: element, role: this.state.roles[champs.size]})
+		}
+		return champs
+	}
 }
 
 export default App;
