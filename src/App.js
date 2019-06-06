cc0cbcb5f9591c22d10ea639bb743d4f4ce7f9df
Lars <lars.manden@gmail.com>
2019-06-06 20:11:03 +0200

    asdsa

--- src/App.js
+++ src/App.js
@@ -13,15 +13,17 @@ class App extends Component {
 
 	render() {
 		let divs = [];
-		this.rollChampions(5).forEach(champion => {
+		let int = 0;
+		this.rollChampions(5).forEach((champion) => {
 			divs.push(
-				<div key={champion.element.name} style={{display:'flex', flexDirection:'column', margin:'1rem'}}>
+				<div key={champion.name} style={{display:'flex', flexDirection:'column', margin:'1rem'}}>
 				
-					<span>{champion.element.name}</span>
-					<img src={`${process.env.PUBLIC_URL}/champion/${champion.element.image.full}`} height='100px' width='100px' alt="champion"></img>
-					<span>{champion.role}</span>
+					<span>{champion.name}</span>
+					<img src={`${process.env.PUBLIC_URL}/champion/${champion.image.full}`} height='100px' width='100px' alt="champion"></img>
+					<span>{this.state.roles[int]}</span>
 				</div>
 			)
+			int++;
 		})
 		return (
 			<div className="App" style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
@@ -37,7 +39,7 @@ class App extends Component {
 			var random = Math.floor(Math.random() * Object.keys(champions.data).length + 1);
 			var key = Object.keys(champions.data)[random];
 			var element = champions.data[key];
-			champs.set(element.name, {element: element, role: this.state.roles[champs.size]})
+			champs.set(element.name, element)
 		}
 		return champs
 	}
