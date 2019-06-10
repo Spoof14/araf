129e89b67badc6a09a9252b4f8c5bcbca573bc2c
Lars <lars.manden@gmail.com>
2019-06-10 21:18:06 +0200

    reroll

--- src/App.js
+++ src/App.js
@@ -7,24 +7,29 @@ class App extends Component {
 		super(props)
 
 		this.state = {
-			roles: ["Top", "Jungle", "Mid", "Bottom", "Support"]
+			roles: ["Top", "Jungle", "Mid", "Bottom", "Support"],
+			randomChampions:[]
 		}
 	}
 
+	componentDidMount(){
+		this.setState({
+			randomChampions:this.rollChampions(5)
+		}) 
+	}
+
 	render() {
-		let divs = [];
-		let int = 0;
-		this.rollChampions(5).forEach((champion) => {
-			divs.push(
-				<div key={champion.name} style={{display:'flex', flexDirection:'column', margin:'1rem'}}>
-				
-					<span>{champion.name}</span>
-					<img src={`${process.env.PUBLIC_URL}/champion/${champion.image.full}`} height='100px' width='100px' alt="champion"></img>
-					<span>{this.state.roles[int]}</span>
+		let {randomChampions} = this.state;
+		let divs = randomChampions.map((champ, index) => {
+			return (
+				<div key={champ.name} onClick={() => this.rerollChampion(index)} style={{ display: 'flex', flexDirection: 'column', margin: '1rem', cursor: 'pointer' }} >
+					<span>{champ.name}</span>
+					<img src={`${process.env.PUBLIC_URL}/champion/${champ.image}`} height='100px' width='100px' alt="champion"></img>
+					<span>{this.state.roles[index]}</span>
 				</div>
 			)
-			int++;
 		})
+		
 		return (
 			<div className="App" style={{display:'flex', flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
 				{divs}
@@ -33,16 +38,43 @@ class App extends Component {
 	}
 
 
-	rollChampions(numberOfChampions) {
-		let champs = new Map();
-		while(champs.size < numberOfChampions){
-			var random = Math.floor(Math.random() * Object.keys(champions.data).length + 1);
-			var key = Object.keys(champions.data)[random];
-			console.log(Object.keys(champions.data).length)
-			var element = champions.data[key];
-			champs.set(element.name, element)
+	rollChampions() {
+		let champs = []
+		while(champs.length < 5){
+			let element = this.rollChampion();
+			
+			if(!champs.some(e => e.name === element.name))
+				champs.push(element)
 		}
 		return champs
+	}
+
+
+
+	rollChampion(){
+		var random = Math.floor(Math.random() * Object.keys(champions.data).length);
+		var key = Object.keys(champions.data)[random];
+		var element = champions.data[key];
+		return {name: element.name, image: element.image.full}
+	}
+
+	someChampIsSame(array, newChamp){
+		return array.some(champ => champ.name === newChamp.name)
+	}
+	
+	rerollChampion(index){
+		let { randomChampions } = this.state;
+		let currChamp = randomChampions[index];
+		let newChamp = currChamp;
+		
+		while(newChamp === currChamp || this.someChampIsSame(randomChampions, newChamp)){
+			newChamp = this.rollChampion()
+		}
+		randomChampions[index] = newChamp
+		this.setState({
+			randomChampions
+		})
+
 	}
 }
 
